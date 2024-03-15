399b3405ad4a6a170c560f35508555ae52019fdd
gorhf9397 <trr456@naver.com>
2024-03-16 00:02:44 +0900

    update function

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const todoList = document.querySelector(".todo");
 
 let arr = [];
 
-function deleteTodo(event) {
-  const li = event.target.parentNode;
+function deleteTodo(event, li) {
+  const parentLi = event.target.parentNode;
 
-  arr = arr.filter((value) => value.id !== Number(li.id));
-  li.remove();
+  arr = arr.filter((value) => value.id !== Number(parentLi.id));
+  parentLi.remove();
 }
 
 function updateTodo(event) {
   const li = event.target.parentNode;
-  const h3Value = li.querySelector("h3").innerText;
+  const h3 = li.querySelector("h3");
+  const h3Value = h3.innerText;
   const input = document.createElement("input");
   const button = document.createElement("button");
+
+  h3.style.display = "none";
+  event.target.style.display = "none";
 
   input.type = "text";
   input.value = h3Value;
@@ -24,7 +28,7 @@ function updateTodo(event) {
   li.appendChild(input);
   li.appendChild(button);
 
-  button.addEventListener("click", rePaintTodo(h3Value));
+  button.addEventListener("click", () => rePaintTodo(h3Value));
 }
 
 function rePaintTodo(h3Value) {
@@ -33,6 +37,7 @@ function rePaintTodo(h3Value) {
 
 function paintTodo(todoObj) {
   let li = document.createElement("li");
+  let div = document.createElement("div");
   let h3 = document.createElement("h3");
   let button = document.createElement("button");
   let button2 = document.createElement("button");
@@ -43,12 +48,13 @@ function paintTodo(todoObj) {
   button2.innerText = "수정";
 
   todoList.appendChild(li);
-  li.appendChild(h3);
-  li.appendChild(button);
-  li.appendChild(button2);
+  li.appendChild(div);
+  div.appendChild(h3);
+  div.appendChild(button);
+  div.appendChild(button2);
 
-  button.addEventListener("click", deleteTodo);
-  button2.addEventListener("click", updateTodo);
+  button.addEventListener("click", () => deleteTodo(li));
+  button2.addEventListener("click", () => updateTodo(li));
 }
 
 function addTodoFnc(event) {
